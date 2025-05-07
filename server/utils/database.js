ae9ff925833c217b6440ef9eb6c25539851a17e1
ambika-chukanatti <67265606+ambika-chukanatti@users.noreply.github.com>
2025-05-07 17:38:18 +0530

    changes in database.js

--- server/utils/database.js
+++ server/utils/database.js
@@ -1,8 +1,15 @@
 import Sequelize from 'sequelize';
 
-const sequelize = new Sequelize('job_tracker_app', process.env.DATABASE_USERNAME, process.env.DATABASE_SECRET, {
-  dialect: 'mysql',
-  host: 'localhost'
-});
+const sequelize = new Sequelize(
+  process.env.DB_NAME,         
+  process.env.DB_USER,         
+  process.env.DB_PASSWORD,     
+  {
+    host: process.env.DB_HOST, 
+    port: process.env.DB_PORT, 
+    dialect: 'mysql',
+    logging: false,           
+  }
+);
 
 export default sequelize;
